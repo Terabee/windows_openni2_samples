18986377cf1949a2230cf22a4249a8aca260170b
Pierre-Louis Kabaradjian <pierre.louis.kaba@gmail.com>
2019-03-26 14:52:58 +0100

    Set zero-distance pixels to black

--- DepthViewer/DepthViewer.cpp
+++ DepthViewer/DepthViewer.cpp
@@ -51,39 +51,60 @@ int main(int argc, char* argv[])
 		if (vsDepth.isValid()) {
 			if (STATUS_OK == vsDepth.readFrame(&depth_frame)) {
 				Mat imgDepth(depth_frame.getHeight(), depth_frame.getWidth(), CV_16UC1, (void*)depth_frame.getData());
+				Mat imgDepthScaled(depth_frame.getHeight(), depth_frame.getWidth(), CV_16UC1);
 				Mat img8bitDepth;
 
 				for (int i = 0; i < depth_frame.getHeight(); i++)
 				{
 					uint16_t * row = imgDepth.ptr<uint16_t>(i);
+					uint16_t * rowScaled = imgDepthScaled.ptr<uint16_t>(i);
 					for (int j = 0; j < depth_frame.getWidth(); j++)
 					{
 						uint16_t* pixel = &row[j];
+						uint16_t* pixelScaled = &rowScaled[j];
 
 						if (*pixel > max_dist) {
-							*pixel = max_dist;
+							*pixelScaled = max_dist;
 						}
 						else if (*pixel < min_dist && *pixel > 0) {
-							*pixel = min_dist;
+							*pixelScaled = min_dist;
 						}
 						else if (*pixel == 0)
 						{
-							*pixel = max_dist;
+							*pixelScaled = max_dist;
 						}
+						else *pixelScaled = *pixel;
+
 						if (reverse_colormap) {
-							*pixel = 65535 - (*pixel - min_dist)*(int)(65535 / (max_dist - min_dist));
+							*pixelScaled = 65535 - (*pixelScaled - min_dist)*(int)(65535 / (max_dist - min_dist));
 						}
-						else *pixel = (*pixel - min_dist)*(int)(65535 / (max_dist - min_dist));
-						pixel++;
+						else *pixelScaled = (*pixelScaled - min_dist)*(int)(65535 / (max_dist - min_dist));
 					}
 				}
 
 				// Convert 16bit to 8bit greyscale
-				imgDepth.convertTo(img8bitDepth, CV_8U, 1.0 / 257.0);
+				imgDepthScaled.convertTo(img8bitDepth, CV_8U, 1.0 / 257.0);
 
 				// Apply color map to 8bit frame
 				Mat rgb(60, 80, CV_8UC3);
 				applyColorMap(img8bitDepth, rgb, COLORMAP_JET);
+
+				// Set invalid pixel to black for any colormap
+				for (int i = 0; i < depth_frame.getHeight(); i++) {
+					uint16_t * row = imgDepth.ptr<uint16_t>(i);
+					for (int j = 0; j < depth_frame.getWidth(); j++) {
+						uint16_t* pixel = &row[j];
+						if (*pixel == 0)
+						{	
+							Vec3b color = rgb.at<Vec3b>(Point(j, i));
+							color[0] = 0;
+							color[1] = 0;
+							color[2] = 0;
+							rgb.at<Vec3b>(Point(j, i)) = color;
+						}
+					}
+				}
+
 				cv::resize(rgb, rgb, cv::Size(), 10, 10, 1);
 				imshow("Depth View", rgb);
 			}
